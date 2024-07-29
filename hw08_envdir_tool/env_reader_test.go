05e82db23a9e3c48874fe8e8e18d6dc440708d28
AndreyChufelin <chufelin.andrey@gmail.com>
2024-07-29 23:34:52 +0300

    HW8 is completed

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,29 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	t.Run("Correct data", func(t *testing.T) {
+		env, err := ReadDir("./testdata/env/")
+
+		require.Equal(t, EnvValue{"\"hello\"", false}, env["HELLO"])
+		require.Equal(t, EnvValue{"bar", false}, env["BAR"], "Only first line")
+		require.Equal(t, EnvValue{"", false}, env["EMPTY"], "Trim")
+		require.Equal(t, EnvValue{"   foo\nwith new line", false}, env["FOO"], "Replace 0x00 to \n")
+		require.Equal(t, EnvValue{"", true}, env["UNSET"], "Remove when empty file")
+
+		require.NotContains(t, env, "WITH=")
+		require.Equal(t, EnvValue{"with =", false}, env["WITH"], "Remove '=' sign from name")
+
+		require.NoError(t, err)
+	})
+	t.Run("Not existing directory", func(t *testing.T) {
+		_, err := ReadDir("./testdata/not")
+
+		require.ErrorIs(t, err, ErrInvalidDir)
+	})
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,44 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("Sets env", func(t *testing.T) {
+		env := Environment{
+			"BAR": EnvValue{
+				Value:      "bar",
+				NeedRemove: false,
+			},
+			"UNSET": EnvValue{
+				Value:      "",
+				NeedRemove: true,
+			},
+			"ADDED": EnvValue{
+				Value:      "from original env",
+				NeedRemove: false,
+			},
+		}
+
+		os.Setenv("BAR", "foo")
+		code := RunCmd([]string{"echo"}, env)
+
+		require.Equal(t, "bar", os.Getenv("BAR"), "Sets variable")
+		require.Equal(t, "from original env", os.Getenv("ADDED"), "Doesn't reset original env")
+
+		_, u := os.LookupEnv("UNSET")
+		require.False(t, u, "Removes variable")
+
+		require.Equal(t, 0, code)
+	})
+	t.Run("Returns exit code", func(t *testing.T) {
+		env := Environment{}
+		code := RunCmd([]string{"false"}, env)
+
+		require.Equal(t, 1, code)
+	})
 }
